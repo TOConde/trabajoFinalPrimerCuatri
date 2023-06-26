8922f3f496e864ebd51c121cd588e14dca0a2571
TOConde <tomy_t.c@hotmail.com>
2023-06-26 16:22:14 -0300

    toques finales de css

--- trabajoFinalParte1.js
+++ trabajoFinalParte1.js
@@ -1,26 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  
+document.addEventListener('DOMContentLoaded', function () {
+
   const inputNombre = document.getElementById("inputNombre");
   const inputEmail = document.getElementById("inputEmail");
   const inputTelefono = document.getElementById("inputTelefono");
   const inputEdad = document.getElementById("inputEdad");
   const inputCategoria = document.getElementById("inputCategoria");
   const btnEnviar = document.getElementById("btnEnviar");
-  
+
   btnEnviar.addEventListener('click', imprimirEnConsola);
-  
+
   function imprimirEnConsola() {
-    const nombre = inputNombre.value;
-    const email = inputEmail.value;
-    const telefono = inputTelefono.value;
-    const edad = inputEdad.value;
-    const categoria = inputCategoria.value;
-  
-    console.log("Nombre:", nombre);
-    console.log("Email:", email);
-    console.log("Telefono:", telefono);
-    console.log("Edad:", edad);
-    console.log("Categoría:", categoria);
+    console.log("Nombre:", inputNombre.value);
+    console.log("Email:", inputEmail.value);
+    console.log("Telefono:", inputTelefono.value);
+    console.log("Edad:", inputEdad.value);
+    console.log("Categoría:", inputCategoria.value);
   }
-  
+
 });
