c99b92177a3dad402d95a160d5d92d608d700088
TOConde <tomy_t.c@hotmail.com>
2023-06-26 19:48:28 -0300

    agrego validaciones de nombre, edad y categoria

--- trabajoFinalParte1.js
+++ trabajoFinalParte1.js
@@ -1,20 +1,50 @@
-document.addEventListener('DOMContentLoaded', function () {
 
-  const inputNombre = document.getElementById("inputNombre");
-  const inputEmail = document.getElementById("inputEmail");
-  const inputTelefono = document.getElementById("inputTelefono");
-  const inputEdad = document.getElementById("inputEdad");
-  const inputCategoria = document.getElementById("inputCategoria");
-  const btnEnviar = document.getElementById("btnEnviar");
+const inputNombre = document.getElementById("inputNombre");
+const inputEmail = document.getElementById("inputEmail");
+const inputTelefono = document.getElementById("inputTelefono");
+const inputEdad = document.getElementById("inputEdad");
+const inputCategoria = document.getElementById("inputCategoria");
 
-  btnEnviar.addEventListener('click', imprimirEnConsola);
+const btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener('click', enviarForm);
 
-  function imprimirEnConsola() {
-    console.log("Nombre:", inputNombre.value);
-    console.log("Email:", inputEmail.value);
-    console.log("Telefono:", inputTelefono.value);
-    console.log("Edad:", inputEdad.value);
-    console.log("Categoría:", inputCategoria.value);
+function enviarForm() {
+  imprimirEnConsola();
+}
+
+function imprimirEnConsola() {
+  const nombre = inputNombre.value;
+  const email = inputEmail.value;
+  const telefono = inputTelefono.value;
+  const edad = inputEdad.value;
+  const categoria = inputCategoria.value;
+   
+  let error = validarDatos(nombre,edad,categoria);
+  if (error) {
+    console.log(error);
+    return
   }
 
-});
+  console.clear();
+  console.log("Nombre:", nombre);
+  console.log("Email:", email);
+  console.log("Telefono:", telefono);
+  console.log("Edad:", edad);
+  console.log("Categoría:", categoria);
+}
+
+function validarDatos(nombre,edad,categoria) {
+  if (!nombre) {
+    return "El campo nombre no debe estar vacío"
+  }
+  if (!edad) {
+    return "El campo edad no debe estar vacío"
+  }
+  if (edad < 18) {
+    return "El usuario debe ser mayor de 18 años"
+  }
+  if (categoria === "Elije una opcion...") {
+    return "Elejí una opcion en categoría"
+  }
+}
+
