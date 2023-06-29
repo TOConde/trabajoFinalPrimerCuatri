201ffb1731cffaa4e6a7a516bef9d21e165d7567
TOConde <tomy_t.c@hotmail.com>
2023-06-29 11:40:21 -0300

    agrego funcionalidad a checkbox + toques finales

--- tFP2ToDoList.js
+++ tFP2ToDoList.js
@@ -19,9 +19,15 @@ function agregarTarea() {
   input.setAttribute("type", "text");
 
   const inputCheck = document.createElement("input");
+  inputCheck.setAttribute("id", "check" + contador);
   inputCheck.setAttribute("for", "tarea" + contador);
   inputCheck.setAttribute("class", "check");
   inputCheck.setAttribute("type", "checkbox");
+
+
+  inputCheck.addEventListener('click', function () {
+    cambiarEstilo(inputCheck);
+  })
 
   const borrar = document.createElement("button");
   const imagenIcono = document.createElement("img");
@@ -31,7 +37,7 @@ function agregarTarea() {
   borrar.setAttribute("id", "btnDeleteTarea" + contador);
   borrar.setAttribute("class", "delete");
 
-  borrar.addEventListener("click", function() {
+  borrar.addEventListener("click", function () {
     borrarUnaTarea(div);
   })
 
@@ -48,9 +54,19 @@ function borrarUnaTarea(tarea) {
 }
 
 function borrarTodo() {
+  contador = 1
   contenedor.innerHTML = '';
 }
 
-function checkear() {
-
+function cambiarEstilo(inputCheck) {
+  const input = document.getElementById(inputCheck.getAttribute("for"));
+  if (inputCheck.checked) {
+    input.style.textDecoration = "line-through";
+    input.style.color = "grey"
+    input.disabled = true;
+  } else {
+    input.style.textDecoration = "";
+    input.style.color = "";
+    input.disabled = false;
+  }
 }
